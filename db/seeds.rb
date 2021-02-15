596d24a38087cb6a8052dd88cefefe0f77875c96
Alexis Flipo <alexis.flipo1@gmail.com>
2021-02-15 10:28:11 +0100

    Addings controllers and methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Category.destroy_all
+puts "Creating Category"
+
+Category.create(name: 'Santé')
+Category.create(name: 'IT & développement')
+Category.create(name: 'Informatique')
+Category.create(name: 'Culture')
+Category.create(name: 'Sport')
+Category.create(name: 'En société')
+Category.create(name: 'Films')
+Category.create(name: 'Politique')
+Category.create(name: 'Voyages')
+puts "Category done"
+puts "Creating Post"
+
+Post.create(title:'Post1', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[0].id)
+Post.create(title:'Post2', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[1].id)
+Post.create(title:'Post3', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[2].id)
+Post.create(title:'Post4', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[3].id)
+Post.create(title:'Post5', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[4].id)
+Post.create(title:'Post6', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[5].id)
+Post.create(title:'Post7', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[6].id)
+Post.create(title:'Post7', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[7].id)
+Post.create(title:'Post8', content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, alias!", category_id:Category.all[8].id)
+puts "Creating Post done"
